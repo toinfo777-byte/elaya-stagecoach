f2c657f90821d2e819aabfc3ecddb1cd67fb1c93
toinfo777-byte <toinfo777@gmail.com>
2025-09-11 18:02:23 +0300

    Update states.py

--- app/bot/states.py
+++ app/bot/states.py
@@ -1,6 +1,6 @@
 # app/bot/states.py
 from aiogram.fsm.state import StatesGroup, State
 
-class Feedback(StatesGroup):
-    WaitRating = State()  # ждём нажатия 🔥/👌/😐 или «1 фраза»
-    WaitText = State()    # ждём текст отзыва или «Пропустить»
+class FeedbackStates(StatesGroup):
+    # ждём короткий текст отзыва
+    wait_text = State()
