0d3733cb7fa4645c66eb73d4a2cb9c06978f5a45
toinfo777-byte <toinfo777@gmail.com>
2025-09-15 12:20:57 +0300

    Update states.py

--- app/bot/states.py
+++ app/bot/states.py
@@ -4,5 +4,6 @@ from aiogram.fsm.state import StatesGroup, State
 class CoachStates(StatesGroup):
     wait_feeling = State()
 
+# ⬇️ НОВОЕ
 class FeedbackStates(StatesGroup):
-    wait_text = State()
+    wait_phrase = State()
