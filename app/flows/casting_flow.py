df63b7cb6aca851a3720b0a704da7c27f0f64de8
toinfo777-byte <toinfo777@gmail.com>
2025-09-22 17:20:46 +0300

    Create casting_flow.py

---
+++ app/flows/casting_flow.py
@@ -0,0 +1,7 @@
+# app/flows/casting_flow.py
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+
+async def start_casting_flow(message: Message, state: FSMContext):
+    # общий сценарий
+    ...
