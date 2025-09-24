7216853e36b4af8751aa59d4ddbe656934ffc940
toinfo777-byte <toinfo777@gmail.com>
2025-09-24 19:21:34 +0300

    Update inline.py

--- app/keyboards/inline.py
+++ app/keyboards/inline.py
@@ -2,7 +2,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def casting_skip_kb() -> InlineKeyboardMarkup:
-    # синхронизировано с хендлером: F.data == "cast:skip_url"
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton(text="Пропустить", callback_data="cast:skip_url")]
