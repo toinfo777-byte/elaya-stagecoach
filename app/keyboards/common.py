6ada5d8801dda7b9325ee76dd5e82b757524c855
toinfo777-byte <toinfo777@gmail.com>
2025-09-25 20:26:01 +0300

    Update common.py

--- app/keyboards/common.py
+++ app/keyboards/common.py
@@ -1,8 +1,8 @@
+# app/keyboards/common.py
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.types import InlineKeyboardMarkup
 
 def menu_btn() -> InlineKeyboardMarkup:
     kb = InlineKeyboardBuilder()
     kb.button(text="🏠 В меню", callback_data="go:menu")
-    kb.adjust(1)
     return kb.as_markup()
