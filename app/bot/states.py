5fbdd8085574dd74824dd4ce0183e44de593fd2e
toinfo777-byte <toinfo777@gmail.com>
2025-10-01 17:24:19 +0300

    Delete app/bot directory

--- app/bot/states.py
+++
@@ -1,8 +0,0 @@
-# app/bot/states.py
-from aiogram.fsm.state import StatesGroup, State
-
-class FeedbackStates(StatesGroup):
-    wait_phrase = State()
-
-class ApplyStates(StatesGroup):
-    wait_text = State()
