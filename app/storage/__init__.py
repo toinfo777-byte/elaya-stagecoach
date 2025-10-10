b97b434aa13997966a25a7f4269e76699d2cce8f
toinfo777-byte <toinfo777@gmail.com>
2025-10-10 17:33:43 +0300

    Update __init__.py

--- app/storage/__init__.py
+++ app/storage/__init__.py
@@ -1,18 +1,2 @@
 # app/storage/__init__.py
-from .repo import (
-    ensure_schema,
-    progress,
-    ProgressRepo,
-    ProgressSummary,
-    save_casting,
-    delete_user,
-)
-
-__all__ = [
-    "ensure_schema",
-    "progress",
-    "ProgressRepo",
-    "ProgressSummary",
-    "save_casting",
-    "delete_user",
-]
+from .repo import *  # re-export на всякий случай
