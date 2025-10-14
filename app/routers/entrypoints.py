0919c4c9bc5ba8e971792ad9ada5ea5aef7b0312
toinfo777-byte <toinfo777@gmail.com>
2025-10-14 14:00:13 +0300

    Update entrypoints.py

--- app/routers/entrypoints.py
+++ app/routers/entrypoints.py
@@ -2,11 +2,15 @@
 from __future__ import annotations
 from aiogram import Router, F
 from aiogram.types import CallbackQuery
+
 from app.routers.help import show_main_menu
 
-go_router = Router(name="entrypoints")
+# Инициализируем основной router
+router = Router(name="entrypoints")
 
-@go_router.callback_query(F.data == "go:menu")
+
+@router.callback_query(F.data == "go:menu")
 async def go_menu(cb: CallbackQuery):
+    """Обработчик кнопки перехода в главное меню"""
     await cb.answer()
     await show_main_menu(cb.message)
