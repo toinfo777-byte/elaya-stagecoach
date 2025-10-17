7555a8023e46dd19eaa0f98aebc49d2f60bc373d
toinfo777-byte <toinfo777@gmail.com>
2025-10-17 17:29:55 +0300

    Update control.py

--- app/routers/control.py
+++ app/routers/control.py
@@ -1,9 +1,6 @@
 from __future__ import annotations
-
 from aiogram import Router
-
 from app.control.commands import router as control_router
 
-# Единая точка подключения команд управления
-router = Router(name="control-root")
+router = Router(name="control_root")
 router.include_router(control_router)
