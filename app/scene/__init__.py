7a5c99c965a0833ea9a9f919521813debb01eb74
toinfo777-byte <toinfo777@gmail.com>
2025-10-30 10:40:48 +0300

    Update __init__.py

--- app/scene/__init__.py
+++ app/scene/__init__.py
@@ -1,3 +1,14 @@
-from . import intro, reflect, transition, manager
+# Удобные реэкспорты
+from . import config  # значения INTRO/REFLECT/TRANSITION
+from . import manager  # SceneManager
+from .intro import router as intro_router
+from .reflect import router as reflect_router
+from .transition import router as transition_router
 
-__all__ = ["intro", "reflect", "transition", "manager"]
+__all__ = [
+    "config",
+    "manager",
+    "intro_router",
+    "reflect_router",
+    "transition_router",
+]
