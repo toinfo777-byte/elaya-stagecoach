73e08a28dc2094bc4191e00be4bee2a7fe758a2b
toinfo777-byte <toinfo777@gmail.com>
2025-11-17 21:13:23 +0300

    Update __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,8 +1,19 @@
+from aiogram import Router
+
 from .start import router as start_router
 from .reviews import router as reviews_router
 from .training import router as training_router
 
+# общий корневой роутер бота
+router = Router(name="root-router")
+
+# порядок важен только логически, для нас так наглядно
+router.include_router(start_router)
+router.include_router(reviews_router)
+router.include_router(training_router)
+
 __all__ = (
+    "router",
     "start_router",
     "reviews_router",
     "training_router",
