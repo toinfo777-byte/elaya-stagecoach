853a93c4aab4375096fed7ab2f5c5947e67fedb2
toinfo777-byte <toinfo777@gmail.com>
2025-11-17 16:22:27 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,14 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-    )
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     TG_BOT_TOKEN: str
-    WEBHOOK_SECRET: str | None = None
-    BASE_URL: str | None = None
+    WEBHOOK_SECRET: str
+    BASE_URL: str
 
 
 settings = Settings()
