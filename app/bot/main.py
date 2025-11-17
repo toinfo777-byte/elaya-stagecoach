90defc792bf04719c14c6d1eb13ec8bc9e0d4cff
toinfo777-byte <toinfo777@gmail.com>
2025-11-17 21:00:36 +0300

    Update main.py

--- app/bot/main.py
+++ app/bot/main.py
@@ -2,14 +2,16 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 
 from app.config import settings
-from app.routers import start, reviews, training
+from app.routers import start_router, reviews_router, training_router
 
 bot = Bot(
     token=settings.TG_BOT_TOKEN,
     default=DefaultBotProperties(parse_mode="HTML"),
 )
+
 dp = Dispatcher()
 
-dp.include_router(start.router)
-dp.include_router(training.router)   # один раз!
-dp.include_router(reviews.router)
+# Каждый router включаем только один раз
+dp.include_router(start_router)
+dp.include_router(training_router)
+dp.include_router(reviews_router)
